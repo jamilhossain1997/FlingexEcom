af851603b9dca948684f9eb4c572a65f941990eb
Md.jamil hossaain <jamilhossain4792@gmail.com>
2023-05-07 20:45:54 +0600

    catgory work done

--- src/widgets/home1/featuredProduct.js
+++ src/widgets/home1/featuredProduct.js
@@ -175,7 +175,7 @@ const featuredProduct = () => {
                                     <Col xs={6} xl={2} lg={2} md={6} >
                                         <Card
                                             style={{
-                                                width: '13rem'
+                                                width: '11rem'
                                             }}
                                             className="mb-1"
                                         >
@@ -248,149 +248,7 @@ const featuredProduct = () => {
                 </Container>
             </div>
 
-            <Modal isOpen={modelview} toggle={modelview} className="view-modal" style={{ maxWidth: '900px', width: '60%' }}>
-                <ToastContainer autoClose={900} />
-                <ModalHeader className="border-bottom-0 pb-0">
-                    <Button color="danger" onClick={() => quickview()} ><span aria-hidden="true">×</span></Button>
-                </ModalHeader>
-                <ModalBody>
 
-                    <Row className="align-items-center">
-
-                        <div className="col-lg-5 col-12">
-                            <img className="img-fluid rounded" src={`${imgUrl}storage/app/public/product/thumbnail/${viewproduct.thumbnail}`} alt="hello" />
-                        </div>
-
-                        <div className="col-lg-7 col-12 mt-5 mt-lg-0">
-                            <div className="product-details">
-                                <h3 className="mb-0" style={{ fontSize: `16px` }}>{viewproduct.name}</h3>
-
-                                <div className="star-rating mb-4"><i className="las la-star" /><i className="las la-star" /><i className="las la-star" /><i className="las la-star" /><i className="las la-star" />
-                                </div>
-                                <span className="product-price h4">
-                                    <span className="product-price">
-
-                                        {
-                                            viewproduct.discount > 0 ? viewproduct?.discount_type == 'percent' ? <> Discount:{Math.round((viewproduct.discount))}%</> : null : null
-                                        }
-
-                                        {
-                                            viewproduct.discount > 0 ? viewproduct?.discount_type == 'flat' ? <> Discount:৳{Math.round((viewproduct.discount / convert))}</> : null : null
-                                        }
-                                    </span><br />
-                                    <span className="product-price">
-                                        {
-                                            viewproduct.discount > 0 ? viewproduct?.discount_type == 'percent' ? <>৳{(Math.round(viewproduct?.unit_price / convert, 2)) - (Math.round(((viewproduct?.unit_price / convert * viewproduct?.discount)), 2) / 100)}</> : <>৳{(Math.round(viewproduct?.unit_price / convert, 2)) - (Math.round((viewproduct?.discount) / convert, 2))}</> : <>৳{Math.round(viewproduct?.unit_price / convert, 2)}</>
-                                        }
-                                        <span>
-                                            {
-                                                viewproduct?.discount > 0 ? <del className="text-muted h6"> ৳{Math.round(viewproduct?.unit_price / convert, 2)}</del>
-                                                    : null
-                                            }
-                                        </span>
-
-                                    </span>
-
-                                </span>
-
-                                <ul className="list-unstyled my-4">
-                                    <li className="mb-2">Availibility: <span className="text-muted"> {viewproduct.current_stock}</span>
-                                    </li>
-                                    {/* <li>Categories :<span className="text-muted"> {viewproduct.category}</span>
-</li> */}
-                                </ul>
-                                <p className="mb-4">{viewproduct.description}</p>
-                                <div className="d-sm-flex align-items-center mb-5">
-                                    <div className="d-flex align-items-center mr-sm-4">
-                                        <button className="btn-product btn-product-up"> <i className="las la-minus" />
-                                        </button>
-                                        <input className="form-product" type="number" name="form-product" defaultValue={1} />
-                                        <button className="btn-product btn-product-down"> <i className="las la-plus" />
-                                        </button>
-                                    </div>
-                                </div>
-                                {/* color & Size */}
-                                <div className="d-sm-flex align-items-center mb-3">
-                                    {viewproduct?.choice_options?.map((sizes, index) => {
-
-                                        return (
-                                            <>
-                                                {
-                                                    sizes.options ? <>
-                                                        <p className='mr-3' style={{ marginTop: `10px` }}>Size:</p>
-                                                        <select className="custom-select mt-3 mt-sm-0" style={{ width: `70px` }}>
-                                                            {sizes.options.map((options, index) =>
-                                                                <option key={index}>{options}</option>)}
-                                                        </select>
-
-                                                    </> : null
-
-                                                }
-                                            </>
-
-                                        )
-
-                                    }
-                                    )}
-                                </div>
-                                <div className="d-flex text-center ml-sm-4 mt-3 mt-sm-0" id="inputGroupSelect02">
-                                    {
-                                        viewproduct?.colors == '' ? <>
-                                        </> : <><span className='mr-3'>Color:</span></>
-                                    }
-                                    {viewproduct?.colors?.map((color, index) => {
-
-                                        return (
-                                            <>
-                                                {
-                                                    color.code ?
-                                                        <> <div className="form-check pl-0 mr-3">
-                                                            <input type="checkbox" value={color.name} id={`color-filter${index}`} className="form-check-input" checked={selectedColor === color.name}
-                                                                onChange={onChangeColor} />
-                                                            <label className="form-check-label" htmlFor={`color-filter${index}`} style={{ background: `${color.code}` }} />
-                                                        </div></> : null
-                                                }
-
-                                            </>
-                                        )
-                                    }
-                                    )}
-                                </div>
-
-                                <div className="d-sm-flex align-items-center mt-5">
-
-                                    {!CartItems(viewproduct.id) ?
-                                        <Link to="#" onClick={() => Productaddcart(viewproduct.id, viewproduct.name, viewproduct.thumbnail, viewproduct.min_qty, (viewproduct.unit_price - (viewproduct?.discount_type == 'percent' ? ((viewproduct?.unit_price * viewproduct?.discount) / 100) : viewproduct.discount)), viewproduct.current_stock)} className="btn btn-primary btn-animated mr-sm-4 mb-3 mb-sm-0" rel="nofollow" ><i className="las la-shopping-cart mr-1" />Add To Cart</Link>
-                                        :
-                                        <Link to="/OneStepCheck" className="btn btn-primary btn-animated mr-sm-4 mb-3 mb-sm-0" rel="nofollow" id="viewcart1"><i className="las la-shopping-cart mr-1" />view cart</Link>
-
-                                    }
-
-
-                                    <Link to="/OneStepCheck" onClick={() => Productaddcart(viewproduct.id, viewproduct.name, viewproduct.thumbnail, viewproduct.min_qty, (viewproduct.unit_price - (viewproduct?.discount_type == 'percent' ? ((viewproduct?.unit_price * viewproduct?.discount) / 100) : viewproduct.discount)), viewproduct.current_stock)} className="btn btn-primary btn-animated mr-sm-4 mb-3 mb-sm-0" rel="nofollow" ><i className="las la-shopping-cart mr-1" />Buy Now</Link>
-
-
-                                </div>
-                                <div className="d-sm-flex align-items-center border-top pt-4 mt-5">
-                                    <h6 className="mb-sm-0 mr-sm-4">Share It:</h6>
-                                    <ul className="list-inline">
-                                        <li className="list-inline-item"><Link className="bg-white shadow-sm rounded p-2" to="#"><i className="la la-facebook" /></Link>
-                                        </li>
-                                        <li className="list-inline-item"><Link className="bg-white shadow-sm rounded p-2" to="#"><i className="la la-dribbble" /></Link>
-                                        </li>
-                                        <li className="list-inline-item"><Link className="bg-white shadow-sm rounded p-2" to="#"><i className="la la-instagram" /></Link>
-                                        </li>
-                                        <li className="list-inline-item"><Link className="bg-white shadow-sm rounded p-2" to="#"><i className="la la-twitter" /></Link>
-                                        </li>
-                                        <li className="list-inline-item"><Link className="bg-white shadow-sm rounded p-2" to="#"><i className="la la-linkedin" /></Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </Row>
-                </ModalBody>
-            </Modal>
         </>
     );
 }
